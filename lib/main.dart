9d0b71c9f7951341351c02fb1a34dcfeb3de9cab
Laim0n <laim0n@yandex.ru>
2019-07-10 04:30:22 +0300

    Redesign MD2

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
+import 'package:flutter/rendering.dart';
+
 import 'ui/tabs/result_tab.dart';
 import 'package:dynamic_theme/dynamic_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
-import 'ui/common/rounded_app_bar.dart';
 import 'ui/tabs/favorite_tab.dart';
 import 'ui/tabs/settings_tab.dart';
 import 'package:md2_tab_indicator/md2_tab_indicator.dart';
+import 'ui/common/custom_appbar.dart' as custom;
 
 void main() => runApp(MyApp());
 
@@ -15,8 +17,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(
       SystemUiOverlayStyle(
-          statusBarColor: Colors.black.withAlpha(2),
-          systemNavigationBarColor: Colors.black.withAlpha(5),
+          statusBarColor: Colors.white.withAlpha(0),
+          systemNavigationBarColor: Colors.white,
           systemNavigationBarIconBrightness: Brightness.dark),
     );
     return DynamicTheme(
@@ -24,10 +26,13 @@ class MyApp extends StatelessWidget {
         data: (brightness) {
           SystemChrome.setSystemUIOverlayStyle(
             SystemUiOverlayStyle(
-                statusBarIconBrightness: brightness,
+                statusBarColor: Colors.white.withAlpha(0),
+                statusBarIconBrightness: brightness == Brightness.light
+                    ? Brightness.dark
+                    : Brightness.light,
                 systemNavigationBarColor: brightness == Brightness.light
                     ? Colors.white
-                    : Colors.grey[850],
+                    : Color(0xFF2E2F32),
                 systemNavigationBarIconBrightness:
                     brightness == Brightness.light
                         ? Brightness.dark
@@ -36,14 +41,22 @@ class MyApp extends StatelessWidget {
           return ThemeData(
             brightness: brightness,
             primaryColor: brightness == Brightness.light
-                ? Colors.pink[300]
-                : Colors.teal[300],
+                ? Color(0xFF1A73E8)
+                : Color(0xFF185ABC),
+            cardColor: brightness == Brightness.light
+                ? Color(0xFFFFFFFF)
+                : Color(0xFF2E2F32),
             bottomAppBarColor: brightness == Brightness.light
                 ? Colors.white
-                : Colors.grey[850],
+                : Color(0xFF2E2F32),
             scaffoldBackgroundColor: brightness == Brightness.light
-                ? Colors.grey[50]
-                : Colors.grey[900],
+                ? Colors.white
+                : Color(0xFF202124),
+            cardTheme: CardTheme(
+              color: brightness == Brightness.light
+                  ? Color(0xFFEEEEEE)
+                  : Color(0xFF1D1E21),
+            ),
             primaryTextTheme: TextTheme(
               body1: TextStyle(color: Colors.white),
             ),
@@ -69,17 +82,17 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage>
     with SingleTickerProviderStateMixin {
-  TabController controller;
+  TabController controllerTab;
 
   @override
   void initState() {
     super.initState();
-    controller = new TabController(length: 3, vsync: this);
+    controllerTab = new TabController(length: 3, vsync: this);
   }
 
   @override
   void dispose() {
-    controller.dispose();
+    controllerTab.dispose();
     super.dispose();
   }
 
@@ -89,17 +102,94 @@ class _MainPageState extends State<MainPage>
     SettingsTab(),
   ];
 
+  Widget infoContainer(double step) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(16, 14, 16, 16),
+      child: Container(
+        decoration: new BoxDecoration(
+          color: Theme.of(context).cardColor,
+          border: Border.all(color: Theme.of(context).cardTheme.color),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withAlpha(15),
+              blurRadius: 3,
+            ),
+          ],
+          borderRadius: BorderRadius.all(Radius.circular(9)),
+        ),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.fromLTRB(18, 6, 18, 6),
+              child: Row(
+                children: <Widget>[
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(top: 2),
+                        child: Text(step.toString(),
+                            style: TextStyle(
+                                fontSize: 15.0,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.black87)),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 3),
+                        child: Text(step.toString(),
+                            style: TextStyle(
+                                fontSize: 13.0,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.black54)),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  double appBarHeight = 150;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: <Widget>[
-          TabBarView(
-            physics: NeverScrollableScrollPhysics(),
-            children: tabs,
-            controller: controller,
+          SafeArea(
+            child: NestedScrollView(
+              headerSliverBuilder:
+                  (BuildContext context, bool innerBoxIsScrolled) {
+                return <Widget>[
+                  custom.SliverAppBar(
+                    pinned: true,
+                    expandedHeight: appBarHeight,
+                    floating: true,
+                    backgroundColor: Colors.transparent,
+                    elevation: 0,
+                    flexibleSpace: LayoutBuilder(
+                      builder:
+                          (BuildContext context, BoxConstraints constraints) {
+                        double step = ((constraints.maxHeight - 84.0) *
+                            100 /
+                            (appBarHeight - 84.0));
+                        return infoContainer(step);
+                      },
+                    ),
+                  ),
+                ];
+              },
+              body: TabBarView(
+                physics: NeverScrollableScrollPhysics(),
+                children: tabs,
+                controller: controllerTab,
+              ),
+            ),
           ),
-          RoundedAppBar(),
           Align(
             alignment: Alignment.bottomCenter,
             child: Container(
@@ -140,7 +230,7 @@ class _MainPageState extends State<MainPage>
                           icon: Icon(MdiIcons.settingsOutline),
                         ),
                       ],
-                      controller: controller,
+                      controller: controllerTab,
                     ),
                     Divider(height: 1)
                   ],
