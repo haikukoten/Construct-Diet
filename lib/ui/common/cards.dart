e35c5bc9b26da1af6552a827a7e7ce6b6258de77
Laim0n <laim0n@yandex.ru>
2019-07-11 23:55:24 +0300

    New cards

---
+++ lib/ui/common/cards.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+
+class Card extends StatelessWidget {
+  final Widget child;
+
+  Card(this.child);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.fromLTRB(0, 5, 0, 5),
+      constraints: BoxConstraints(minHeight: 70),
+      padding: EdgeInsets.fromLTRB(18, 14.5, 18, 14.5),
+      decoration: new BoxDecoration(
+        color: Theme.of(context).cardColor,
+        border: Border.all(color: Theme.of(context).dividerColor),
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+      ),
+      child: Center(child: child),
+    );
+  }
+}
+
+class CardInformation extends StatelessWidget {
+  final String title;
+  final String description;
+  final IconData icon;
+
+  CardInformation(this.title, [this.description, this.icon]);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(Row(
+      children: <Widget>[
+        Container(
+            margin: EdgeInsets.only(right: 15),
+            child: Icon(
+              icon,
+              size: 20,
+              color: Theme.of(context).primaryColor,
+            )),
+        Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Text(
+            title,
+            style: TextStyle(
+              fontSize: description != null ? 15 : 15.5,
+              fontWeight: FontWeight.w600,
+              color: Theme.of(context).textTheme.caption.color,
+            ),
+          ),
+          description != null
+              ? Padding(
+                  padding: EdgeInsets.only(top: 3),
+                  child: Text(
+                    description,
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Theme.of(context)
+                          .textTheme
+                          .caption
+                          .color
+                          .withAlpha(200),
+                    ),
+                  ),
+                )
+              : Container()
+        ]),
+      ],
+    ));
+  }
+}
+
+class CardPlug extends StatelessWidget {
+  final String title;
+  final String description;
+  final IconData icon;
+
+  CardPlug(this.title, [this.description, this.icon]);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(Container(
+      padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+      child: Column(
+        children: <Widget>[
+          Container(
+              margin: EdgeInsets.only(bottom: 15),
+              child: Icon(
+                icon,
+                size: 40,
+                color: Theme.of(context).primaryColor,
+              )),
+          Column(children: [
+            Text(
+              title,
+              style: TextStyle(
+                fontSize: description != null ? 15 : 15.5,
+                fontWeight: FontWeight.w600,
+                color: Theme.of(context).textTheme.caption.color,
+              ),
+            ),
+            description != null
+                ? Padding(
+                    padding: EdgeInsets.only(top: 3),
+                    child: Text(
+                      description,
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Theme.of(context)
+                            .textTheme
+                            .caption
+                            .color
+                            .withAlpha(200),
+                      ),
+                    ),
+                  )
+                : Container()
+          ]),
+        ],
+      ),
+    ));
+  }
+}
