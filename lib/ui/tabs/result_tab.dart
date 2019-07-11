89c5b976036f0df5a45623b9e43c001de5018e1b
Laim0n <laim0n@yandex.ru>
2019-07-11 23:55:35 +0300

    Update result_tab.dart

--- lib/ui/tabs/result_tab.dart
+++ lib/ui/tabs/result_tab.dart
@@ -1,4 +1,6 @@
+import 'package:construct_diet/ui/common/cards.dart' as custom;
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class ResultTab extends StatefulWidget {
   @override
@@ -6,44 +8,18 @@ class ResultTab extends StatefulWidget {
 }
 
 class _ResultTabState extends State<ResultTab> {
-  Widget block(int index) {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 10),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text("хуй намбар " + index.toString(),
-              style: TextStyle(
-                fontSize: 15.0,
-                fontWeight: FontWeight.w400,
-                color: Theme.of(context).textTheme.caption.color,
-              )),
-          Text("хуй хуй ?",
-              style: TextStyle(
-                fontSize: 13.0,
-                fontWeight: FontWeight.w400,
-                color: Theme.of(context).textTheme.caption.color.withAlpha(200),
-              )),
-        ],
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       physics: BouncingScrollPhysics(),
       child: new ConstrainedBox(
         constraints: new BoxConstraints(),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: List.generate(
-            100,
-            (int index) {
-              return block(index);
-            },
-          ),
-        ),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          custom.CardInformation("Укажите свои параметры",
+              "Нажмите на карточку выше для редактирования", MdiIcons.creation),
+          custom.CardPlug("Диеты не найдены", "Нет необходимости в похудении",
+              MdiIcons.magnify)
+        ]),
       ),
     );
   }
