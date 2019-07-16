fc6fd6e08324f20a262b8f5fcc0b3e1969d252ca
Laim0n <laim0n@yandex.ru>
2019-07-16 17:42:50 +0300

    New route animation (TransitionPageRoute)

---
+++ lib/ui/common/page_transition.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class TransitionPageRoute extends PageRouteBuilder {
+  final Widget widget;
+  TransitionPageRoute({this.widget})
+      : super(
+          pageBuilder: (BuildContext context, Animation<double> animation,
+              Animation<double> secondaryAnimation) {
+            return widget;
+          },
+          transitionsBuilder: (context, animation1, animation2, child) {
+            return FadeTransition(
+                opacity:
+                    animation1.drive(Tween<double>(begin: 0.0, end: 1.0).chain(
+                  CurveTween(
+                    curve: Interval(0.0, 0.5),
+                  ),
+                )),
+                child: child);
+          },
+          transitionDuration: Duration(milliseconds: 400),
+        );
+}
