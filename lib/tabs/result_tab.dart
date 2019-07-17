5abe94d5b6ff9d575a76f542f6701a8c6b836356
Laim0n <laim0n@yandex.ru>
2019-07-17 11:37:58 +0300

    Added work in ResultTab

--- lib/tabs/result_tab.dart
+++ lib/tabs/result_tab.dart
@@ -1,8 +1,10 @@
 import 'package:construct_diet/common/cards.dart' as custom;
 import 'package:construct_diet/common/labels.dart';
 import 'package:construct_diet/common/tab_body.dart';
+import 'package:construct_diet/scoped_models/data_model.dart';
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 class ResultTab extends StatefulWidget {
   @override
@@ -13,28 +15,44 @@ class _ResultTabState extends State<ResultTab> {
   @override
   Widget build(BuildContext context) {
     return TabBody(
-      Column(children: [
-        /*custom.Card(
-          TitleLabel(
-            "Сведения о здоровье",
-            icon: Icons.info_outline,
-            child: Column(children: [
-              Divider(
-                height: 5,
-                color: Colors.transparent,
-              ),
-              InfoLabel("Индекс массы тела", description: "24.75"),
-              InfoLabel("Допустимый вес", description: "от 62 кг до 77 кг"),
-              InfoLabel("Избыточный вес", description: "3 кг")
-            ]),
+      ScopedModelDescendant<DataModel>(builder: (context, child, model) {
+        return Column(children: [
+          model.isSet
+              ? custom.Card(
+                  TitleLabel(
+                    "Сведения о здоровье",
+                    icon: Icons.info_outline,
+                    child: Column(children: [
+                      Divider(
+                        height: 5,
+                        color: Colors.transparent,
+                      ),
+                      Column(
+                        children: <Widget>[
+                          InfoLabel("Индекс массы тела",
+                              description: model.imt.toStringAsFixed(2)),
+                          InfoLabel("Допустимый вес",
+                              description:
+                                  "от ${model.minWeight} кг до ${model.maxWeight} кг (идеально – ${model.idealWeight} кг)"),
+                          InfoLabel("Избыточный вес",
+                              description: model.overweight == 0
+                                  ? 'Отсутствует'
+                                  : (model.overweight.toString() + "кг"))
+                        ],
+                      ),
+                    ]),
+                  ),
+                )
+              : Container(),
+          custom.Card(
+            PlugLabel("Диеты не найдены",
+                description: model.overweight < 4
+                    ? "Нет необходимости в похудении."
+                    : "Попробуйте изменить фильтр по предпочтениям.",
+                icon: MdiIcons.magnify),
           ),
-        ),*/
-        custom.Card(
-          PlugLabel("Диеты не найдены",
-              description: "Нет необходимости в похудении.",
-              icon: MdiIcons.magnify),
-        ),
-      ]),
+        ]);
+      }),
     );
   }
 }
