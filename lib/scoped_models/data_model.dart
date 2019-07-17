8c7d4d3a9af03bf0a5e3a08251df76c0c04c754e
Laim0n <laim0n@yandex.ru>
2019-07-17 03:10:38 +0300

    Update DataModel

--- lib/scoped_models/data_model.dart
+++ lib/scoped_models/data_model.dart
@@ -4,11 +4,23 @@ class DataModel extends Model {
   bool _isSet = false;
   bool get isSet => _isSet;
 
+  int _genderIndex = 0;
+  int _ageIndex = 0;
+  int _heightIndex = 0;
+  int _weightIndex = 0;
+  int _wristIndex = 0;
+
   bool _isWoman = true;
   int _age = 18;
   int _height = 165;
   int _weight = 80;
   int _wrist;
+
+  int get genderIndex => _genderIndex;
+  int get ageIndex => _ageIndex;
+  int get heightIndex => _heightIndex;
+  int get weightIndex => _weightIndex;
+  int get wristIndex => _wristIndex;
 
   bool get isWoman => _isWoman;
   int get age => _age;
@@ -16,7 +28,33 @@ class DataModel extends Model {
   int get weight => _weight;
   int get wrist => _wrist;
 
-  void increment() {
+  void setGender(dynamic value, int index) {
+    _isWoman = index == 0;
+    _genderIndex = index;
+    save();
+  }
+
+  void setAge(int value, int index) {
+    _age = value;
+    _ageIndex = index;
+    save();
+  }
+
+  void setHeight(int value, int index) {
+    _height = value;
+    _heightIndex = index;
+    save();
+  }
+
+  void setWeight(int value, int index) {
+    _weight = value;
+    _weightIndex = index;
+    save();
+  }
+
+  void setWrist(int value, int index) {
+    _wrist = value;
+    _wristIndex = index;
     save();
   }
 
