03f36a0872dd2680ba833a430cbf7ff3dedebc41
Laim0n <laim0n@yandex.ru>
2019-07-17 11:37:00 +0300

    Update default values and new huinya

--- lib/scoped_models/data_model.dart
+++ lib/scoped_models/data_model.dart
@@ -7,45 +7,34 @@ class DataModel extends Model {
   int _genderIndex = 0;
   int _ageIndex = 13;
   int _heightIndex = 60;
-  int _weightIndex = 35;
-  int _wristIndex = 2;
+  int _weightIndex = 20;
 
   bool _isWoman = true;
   int _age = 25;
   int _height = 160;
-  int _weight = 65;
-  int _wrist = 15;
+  int _weight = 50;
 
   int get genderIndex => _genderIndex;
   int get ageIndex => _ageIndex;
   int get heightIndex => _heightIndex;
   int get weightIndex => _weightIndex;
-  int get wristIndex => _wristIndex;
 
   bool get isWoman => _isWoman;
   int get age => _age;
   int get height => _height;
   int get weight => _weight;
-  dynamic get wrist {
-    dynamic value = _wrist;
-    if (_wrist == 14 && isWoman) {
-      value = "Меньше 15";
-    }
 
-    if (_wrist == 18 && isWoman) {
-      value = "Больше 17";
-    }
+  int _idealWeight = 0;
+  int _minWeight = 0;
+  int _maxWeight = 0;
+  int _overweight = 0;
+  double _imt = 0;
 
-    if (_wrist == 17 && !isWoman) {
-      value = "Меньше 18";
-    }
-
-    if (_wrist == 21 && !isWoman) {
-      value = "Больше 20";
-    }
-
-    return value;
-  }
+  int get idealWeight => _idealWeight;
+  int get minWeight => _minWeight;
+  int get maxWeight => _maxWeight;
+  int get overweight => _overweight;
+  double get imt => _imt;
 
   void setGender(dynamic value, int index) {
     _isWoman = index == 0;
@@ -71,14 +60,35 @@ class DataModel extends Model {
     save();
   }
 
-  void setWrist(int value, int index) {
-    _wrist = value;
-    _wristIndex = index;
-    save();
+  getStatus() {
+    if (imt < 16) return 0;
+    if (imt >= 16 && imt < 18.5) return 1;
+    if (imt >= 18.5 && imt < 25) return 2;
+    if (imt >= 25 && imt < 30) return 3;
+    if (imt >= 30 && imt < 35) return 4;
+    if (imt >= 35 && imt <= 40) return 5;
+    if (imt > 40) return 6;
   }
 
   void save() {
     _isSet = true;
+    int heightft = ((height * 0.3937008) / 12).floor();
+    double heightinch = ((height * 0.3937008) % 12);
+
+    _idealWeight = isWoman
+        ? (45.5 + ((((heightft * 12) + heightinch) - 60) * 2.3)).floor()
+        : (50 + ((((heightft * 12) + heightinch) - 60) * 2.3)).floor();
+
+    var inchConv = ((heightft * 12) + heightinch) / 39.373533;
+    _minWeight = (inchConv * inchConv * 18.5).floor() + 1;
+    _maxWeight = (inchConv * inchConv * 25).floor();
+
+    var heightm = height * 0.01;
+    _imt = weight / (heightm * heightm);
+
+    _overweight = weight - idealWeight;
+    if (_overweight < 0) _overweight = 0;
+
     notifyListeners();
   }
 }
