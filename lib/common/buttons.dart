76f0319cd12be459f869bc33a234d563652246c0
Laim0n <laim0n@yandex.ru>
2019-07-19 23:51:02 +0300

    New button (IconSwitch)

--- lib/common/buttons.dart
+++ lib/common/buttons.dart
@@ -23,3 +23,42 @@ class IconButton extends StatelessWidget {
     );
   }
 }
+
+class IconSwitch extends StatefulWidget {
+  final double width;
+  final double height;
+  final Icon icon;
+  final Icon selectedIcon;
+  final bool value;
+  final ValueChanged<bool> onChanged;
+
+  IconSwitch(
+      {this.width = 40,
+      this.height = 40,
+      this.icon,
+      this.selectedIcon,
+      this.value,
+      this.onChanged});
+
+  @override
+  _IconSwitchState createState() => _IconSwitchState();
+}
+
+class _IconSwitchState extends State<IconSwitch> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: widget.width,
+      height: widget.height,
+      child: InkWell(
+        highlightColor: Colors.grey.withAlpha(50),
+        splashColor: Colors.grey.withAlpha(50),
+        borderRadius: BorderRadius.all(Radius.circular(50)),
+        child: !widget.value ? widget.icon : widget.selectedIcon,
+        onTap: () {
+          widget.onChanged(!widget.value);
+        },
+      ),
+    );
+  }
+}
