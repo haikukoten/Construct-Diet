20e85d3610fdf3676f5ba57332466ce2f8d5f157
Laim0n <laim0n@yandex.ru>
2019-07-19 23:51:31 +0300

    Add favorites

--- lib/tabs/favorite_tab.dart
+++ lib/tabs/favorite_tab.dart
@@ -9,7 +9,28 @@ class FavoritesTab extends StatefulWidget {
   _FavoritesTabState createState() => _FavoritesTabState();
 }
 
+class FavoriteFood {
+  final int id;
+  final String name;
+  final IconData icon;
+  int isLiked;
+
+  FavoriteFood(this.id, this.name, this.icon, [this.isLiked = 0]);
+}
+
 class _FavoritesTabState extends State<FavoritesTab> {
+  List<FavoriteFood> foodList = <FavoriteFood>[
+    FavoriteFood(1, 'Мясные продукты', MdiIcons.sausage),
+    FavoriteFood(2, 'Рыбные продукты', MdiIcons.fish),
+    FavoriteFood(3, 'Фрукты', MdiIcons.foodApple),
+    FavoriteFood(4, 'Овощи', MdiIcons.corn),
+    FavoriteFood(5, 'Крупы', MdiIcons.rice),
+    FavoriteFood(9, 'Цитрусовые', MdiIcons.circleSlice8),
+    FavoriteFood(7, 'Глютен', MdiIcons.barley),
+    FavoriteFood(8, 'Лактоза', MdiIcons.beer),
+    FavoriteFood(10, 'Глюкоза', MdiIcons.candycane)
+  ];
+
   @override
   Widget build(BuildContext context) {
     return TabBody(
@@ -18,8 +39,30 @@ class _FavoritesTabState extends State<FavoritesTab> {
             description:
                 "Выберите, какие продукты вы желаете видеть в диетах, а какие нужно отсеить из выдачи.",
             icon: MdiIcons.heart)),
-        custom.Card(PlugLabel("Здесь ничего нет",
-            description: "Раздел в разработке.", icon: MdiIcons.help))
+        custom.Card(Column(
+          children: List<Widget>.generate(foodList.length, (int i) {
+            FavoriteFood food = foodList[i];
+            return Column(
+              children: <Widget>[
+                i == 0
+                    ? Container(
+                        height: 1,
+                      )
+                    : Divider(
+                        height: 0,
+                      ),
+                SelectFavoriteLabel(
+                  food.name,
+                  icon: food.icon,
+                  isLiked: food.isLiked,
+                  onChanged: (value) {
+                    print(value);
+                  },
+                ),
+              ],
+            );
+          }),
+        ))
       ]),
     );
   }
