41b60b17f7bdd7032175646ffa8b0d10497cb364
Laim0n <laim0n@yandex.ru>
2019-07-20 11:38:45 +0300

    Update labels.dart

--- lib/common/labels.dart
+++ lib/common/labels.dart
@@ -1,7 +1,200 @@
+import 'package:construct_diet/common/page_transition.dart';
+import 'package:construct_diet/screens/diet_info_page.dart';
+import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 import 'buttons.dart' as custom;
-import 'package:flutter/material.dart';
+
+class ButtonLabel extends StatelessWidget {
+  final String title;
+  final String description;
+  final IconData icon;
+  final Color color;
+  final VoidCallback onPressed;
+
+  ButtonLabel(this.title,
+      {this.description, this.icon, this.color, this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(8, 5, 8, 5),
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          highlightColor: Colors.grey.withAlpha(30),
+          splashColor: Colors.grey.withAlpha(30),
+          onTap: onPressed,
+          borderRadius: BorderRadius.all(Radius.circular(5)),
+          child: Padding(
+            padding: EdgeInsets.all(10),
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                Row(
+                  children: <Widget>[
+                    icon != null
+                        ? Container(
+                            margin: EdgeInsets.only(right: 15),
+                            child: Icon(
+                              icon,
+                              size: 20,
+                              color: color == null
+                                  ? Theme.of(context).primaryColor
+                                  : color,
+                            ))
+                        : Container(),
+                    Container(
+                      width: MediaQuery.of(context).size.width > 750
+                          ? 750.0 - 140.0
+                          : MediaQuery.of(context).size.width - 140,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            title,
+                            strutStyle: StrutStyle(
+                              leading: 0,
+                            ),
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w600,
+                              color: Theme.of(context).textTheme.caption.color,
+                            ),
+                          ),
+                          description != null
+                              ? Padding(
+                                  padding: EdgeInsets.only(top: 3.2),
+                                  child: Text(
+                                    description,
+                                    style: TextStyle(
+                                      fontSize: 12.2,
+                                      color: Theme.of(context)
+                                          .textTheme
+                                          .caption
+                                          .color
+                                          .withAlpha(180),
+                                    ),
+                                  ),
+                                )
+                              : Container()
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Icon(
+                    Icons.chevron_right,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class DietLabel extends StatelessWidget {
+  final String title;
+  final String description;
+  final List<IconData> icons;
+
+  DietLabel(this.title, {this.description, this.icons});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: InkWell(
+          highlightColor: Colors.grey.withAlpha(30),
+          splashColor: Colors.grey.withAlpha(30),
+          onTap: () {
+            Navigator.push(
+              context,
+              TransitionPageRoute(widget: DietInfoPage(title, description)),
+            );
+          },
+          borderRadius: BorderRadius.all(Radius.circular(5)),
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(10, 6.5, 2, 6.5),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding:
+                          EdgeInsets.only(top: description != null ? 0 : 4),
+                      child: Hero(
+                        tag: "title_diet",
+                        child: Text(
+                          title,
+                          strutStyle: StrutStyle(
+                            leading: 0,
+                          ),
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w600,
+                            color: Theme.of(context).textTheme.caption.color,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 3.2),
+                      child: Text(
+                        description,
+                        style: TextStyle(
+                          fontSize: 12.2,
+                          color: Theme.of(context)
+                              .textTheme
+                              .caption
+                              .color
+                              .withAlpha(180),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 14.5),
+                  child: Row(
+                    children: List<Widget>.generate(
+                      icons.length,
+                      (i) {
+                        return Padding(
+                          padding: EdgeInsets.all(3),
+                          child: Icon(
+                            icons[i],
+                            size: 12.2,
+                            color: Theme.of(context)
+                                .textTheme
+                                .caption
+                                .color
+                                .withAlpha(200),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
 
 class InfoLabel extends StatelessWidget {
   final String title;
@@ -75,6 +268,21 @@ class InfoLabel extends StatelessWidget {
   }
 }
 
+class InfoSwitchLabel extends StatefulWidget {
+  final String title;
+  final String description;
+  final IconData icon;
+  final Color color;
+  final bool value;
+  final ValueChanged<bool> onChanged;
+
+  InfoSwitchLabel(this.title,
+      {this.description, this.icon, this.color, this.value, this.onChanged});
+
+  @override
+  _InfoSwitchLabelState createState() => _InfoSwitchLabelState(this.value);
+}
+
 class PlugLabel extends StatelessWidget {
   final String title;
   final String description;
@@ -136,6 +344,18 @@ class PlugLabel extends StatelessWidget {
   }
 }
 
+class SelectFavoriteLabel extends StatefulWidget {
+  final String title;
+  final IconData icon;
+  final int isLiked;
+  final ValueChanged<int> onChanged;
+
+  SelectFavoriteLabel(this.title, {this.icon, this.isLiked, this.onChanged});
+
+  @override
+  _SelectFavoriteLabelState createState() => _SelectFavoriteLabelState();
+}
+
 class TitleLabel extends StatelessWidget {
   final String title;
   final IconData icon;
@@ -193,21 +413,6 @@ class TitleLabel extends StatelessWidget {
       ],
     );
   }
-}
-
-class InfoSwitchLabel extends StatefulWidget {
-  final String title;
-  final String description;
-  final IconData icon;
-  final Color color;
-  final bool value;
-  final ValueChanged<bool> onChanged;
-
-  InfoSwitchLabel(this.title,
-      {this.description, this.icon, this.color, this.value, this.onChanged});
-
-  @override
-  _InfoSwitchLabelState createState() => _InfoSwitchLabelState(this.value);
 }
 
 class _InfoSwitchLabelState extends State<InfoSwitchLabel> {
@@ -305,120 +510,8 @@ class _InfoSwitchLabelState extends State<InfoSwitchLabel> {
   }
 }
 
-class ButtonLabel extends StatelessWidget {
-  final String title;
-  final String description;
-  final IconData icon;
-  final Color color;
-  final VoidCallback onPressed;
-
-  ButtonLabel(this.title,
-      {this.description, this.icon, this.color, this.onPressed});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.fromLTRB(8, 5, 8, 5),
-      child: Material(
-        color: Colors.transparent,
-        child: InkWell(
-          highlightColor: Colors.grey.withAlpha(30),
-          splashColor: Colors.grey.withAlpha(30),
-          onTap: onPressed,
-          borderRadius: BorderRadius.all(Radius.circular(5)),
-          child: Padding(
-            padding: EdgeInsets.all(10),
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                Row(
-                  children: <Widget>[
-                    icon != null
-                        ? Container(
-                            margin: EdgeInsets.only(right: 15),
-                            child: Icon(
-                              icon,
-                              size: 20,
-                              color: color == null
-                                  ? Theme.of(context).primaryColor
-                                  : color,
-                            ))
-                        : Container(),
-                    Container(
-                      width: MediaQuery.of(context).size.width > 750
-                          ? 750.0 - 140.0
-                          : MediaQuery.of(context).size.width - 140,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            title,
-                            strutStyle: StrutStyle(
-                              leading: 0,
-                            ),
-                            style: TextStyle(
-                              fontSize: 15,
-                              fontWeight: FontWeight.w600,
-                              color: Theme.of(context).textTheme.caption.color,
-                            ),
-                          ),
-                          description != null
-                              ? Padding(
-                                  padding: EdgeInsets.only(top: 3.2),
-                                  child: Text(
-                                    description,
-                                    style: TextStyle(
-                                      fontSize: 12.2,
-                                      color: Theme.of(context)
-                                          .textTheme
-                                          .caption
-                                          .color
-                                          .withAlpha(180),
-                                    ),
-                                  ),
-                                )
-                              : Container()
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: Icon(
-                    Icons.chevron_right,
-                  ),
-                )
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class SelectFavoriteLabel extends StatefulWidget {
-  final String title;
-  final IconData icon;
-  final int isLiked;
-  final ValueChanged<int> onChanged;
-
-  SelectFavoriteLabel(this.title, {this.icon, this.isLiked, this.onChanged});
-
-  @override
-  _SelectFavoriteLabelState createState() => _SelectFavoriteLabelState();
-}
-
 class _SelectFavoriteLabelState extends State<SelectFavoriteLabel> {
   int isLiked;
-
-  @override
-  void initState() {
-    super.initState();
-    isLiked = widget.isLiked;
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -528,82 +621,10 @@ class _SelectFavoriteLabelState extends State<SelectFavoriteLabel> {
       ),
     );
   }
-}
-
-class DietLabel extends StatelessWidget {
-  final String title;
-  final String description;
-  final List<IconData> icons;
-
-  DietLabel(this.title, {this.description, this.icons});
 
   @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: description != null
-          ? EdgeInsets.fromLTRB(18, 14.5, 18, 14.5)
-          : EdgeInsets.fromLTRB(18, 19.5, 18, 19.5),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: EdgeInsets.only(top: description != null ? 0 : 4),
-                child: Text(
-                  title,
-                  strutStyle: StrutStyle(
-                    leading: 0,
-                  ),
-                  style: TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.w600,
-                    color: Theme.of(context).textTheme.caption.color,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 3.2),
-                child: Text(
-                  description,
-                  style: TextStyle(
-                    fontSize: 12.2,
-                    color: Theme.of(context)
-                        .textTheme
-                        .caption
-                        .color
-                        .withAlpha(180),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Padding(
-            padding: EdgeInsets.only(bottom: 14.5),
-            child: Row(
-              children: List<Widget>.generate(
-                icons.length,
-                (i) {
-                  return Padding(
-                    padding: EdgeInsets.all(3),
-                    child: Icon(
-                      icons[i],
-                      size: 12.2,
-                      color: Theme.of(context)
-                          .textTheme
-                          .caption
-                          .color
-                          .withAlpha(200),
-                    ),
-                  );
-                },
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
+  void initState() {
+    super.initState();
+    isLiked = widget.isLiked;
   }
 }
