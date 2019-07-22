078cda8eb67502e760b9023e608774122ea80e71
Laim0n <laim0n@yandex.ru>
2019-07-22 10:29:13 +0300

    Update Card

--- lib/common/cards.dart
+++ lib/common/cards.dart
@@ -10,12 +10,18 @@ class Card extends StatelessWidget {
     return Container(
       margin: EdgeInsets.fromLTRB(0, 5, 0, 5),
       constraints: BoxConstraints(minHeight: 70),
-      decoration: new BoxDecoration(
+      decoration: BoxDecoration(
         color: Theme.of(context).cardColor,
         border: Border.all(color: Theme.of(context).dividerColor),
         borderRadius: BorderRadius.all(Radius.circular(10)),
       ),
-      child: Center(child: child),
+      child: Material(
+        color: Colors.transparent,
+        child: Center(child: child),
+      ),
+    );
+  }
+}
     );
   }
 }
