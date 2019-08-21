4fabaa3629eb00f92d6a1756eb78d92b80287a03
Semyon Butenko <laim0n@yandex.ru>
2019-08-21 22:30:53 +0300

    Update dialogs

--- lib/common/dialogs.dart
+++ lib/common/dialogs.dart
@@ -3,16 +3,29 @@ import 'package:construct_diet/globalization/vocabulary.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-Future<String> showDialog<String>(
+Future<String> showDialog<String>(String title,
+    {@required BuildContext context,
+    String description,
+    List<Widget> actions}) {
+  title = description;
+  return showCupertinoDialog<String>(
+      context: context,
+      builder: (BuildContext context) =>
+          Dialog(title, description: description, actions: actions));
+}
+
+Future<String> showSelectionDialog<String>(
     {@required BuildContext context, List<Widget> actions}) {
   return showCupertinoDialog<String>(
       context: context,
-      builder: (BuildContext context) => Dialog(actions: actions));
+      builder: (BuildContext context) => SelectionDialog(actions: actions));
 }
 
 class Dialog extends StatelessWidget {
+  final dynamic title;
+  final dynamic description;
   final List<Widget> actions;
-  Dialog({this.actions});
+  Dialog(this.title, {this.description, this.actions});
 
   @override
   Widget build(BuildContext context) {
@@ -35,7 +48,44 @@ class Dialog extends StatelessWidget {
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
+              Divider(height: 1),
               Expanded(
+                child: ListView(children: actions),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SelectionDialog extends StatelessWidget {
+  final List<Widget> actions;
+  SelectionDialog({this.actions});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: 270,
+        constraints: BoxConstraints(maxHeight: 365),
+        decoration: BoxDecoration(
+          color: Theme.of(context).cardColor,
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                blurRadius: 8,
+                offset: Offset(0, 3)),
+          ],
+        ),
+        child: Material(
+          color: Colors.transparent,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Flexible(
                 child: ListView(children: actions, padding: EdgeInsets.zero),
               ),
               Divider(height: 1),
