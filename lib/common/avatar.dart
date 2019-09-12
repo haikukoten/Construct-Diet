2078fdeb4129bcec78c3bc870653ac7ddd4e97fa
Semyon Butenko <laim0n@yandex.ru>
2019-09-12 16:59:00 +0300

    New component (Avatar)

---
+++ lib/common/avatar.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+class Avatar extends StatefulWidget {
+  final String url;
+  final double size;
+  Avatar({this.url, this.size});
+
+  @override
+  _AvatarState createState() => _AvatarState();
+}
+
+class _AvatarState extends State<Avatar> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: widget.size,
+      width: widget.size,
+      child: ClipOval(
+        child: Stack(
+          children: <Widget>[
+            Container(color: Theme.of(context).dividerColor),
+            FadeInImage.memoryNetwork(
+              fadeInDuration: Duration(milliseconds: 150),
+              placeholder: kTransparentImage,
+              image: widget.url,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
