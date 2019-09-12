c7646a3550a431758e57d6e8c98bc103ebfee2b3
Semyon Butenko <laim0n@yandex.ru>
2019-09-12 17:25:44 +0300

    Update ContributorsPage

--- lib/screens/contributors_page.dart
+++ lib/screens/contributors_page.dart
@@ -1,3 +1,4 @@
+import 'package:construct_diet/common/avatar.dart';
 import 'package:construct_diet/common/buttons.dart' as custom;
 import 'package:construct_diet/common/cards.dart' as custom;
 import 'package:construct_diet/common/labels.dart';
@@ -26,7 +27,7 @@ class _ContributorsPageState extends State<ContributorsPage>
 
   Widget appBar(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.fromLTRB(17, 16, 17, 17),
+      padding: EdgeInsets.fromLTRB(17, 16, 17, 5),
       child: Hero(
         tag: 'appbar',
         child: Material(
@@ -34,71 +35,85 @@ class _ContributorsPageState extends State<ContributorsPage>
           shadowColor: Theme.of(context).cardTheme.color,
           borderRadius: BorderRadius.all(Radius.circular(8.5)),
           color: Theme.of(context).cardColor,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          child: Stack(
             children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.fromLTRB(8, 5, 8, 5),
-                child: Row(
-                  children: <Widget>[
-                    custom.IconButton(
-                      icon: Theme.of(context).platform == TargetPlatform.iOS
-                          ? Icons.arrow_back_ios
-                          : Icons.arrow_back,
-                      iconSize: Theme.of(context).platform == TargetPlatform.iOS
-                          ? 18
-                          : 22,
-                      onPressed: () => Navigator.pop(context),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(8, 5, 8, 5),
+                    child: Row(
+                      children: <Widget>[
+                        custom.IconButton(
+                          icon: Theme.of(context).platform == TargetPlatform.iOS
+                              ? Icons.arrow_back_ios
+                              : Icons.arrow_back,
+                          iconSize:
+                              Theme.of(context).platform == TargetPlatform.iOS
+                                  ? 18
+                                  : 22,
+                          onPressed: () => Navigator.pop(context),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(5, 0, 0, 0),
+                          child: Text(
+                            Vocabluary.getWord('Project Contributors'),
+                            style: Theme.of(context).textTheme.caption,
+                          ),
+                        ),
+                      ],
                     ),
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(5, 0, 0, 0),
-                      child: Text(
-                        Vocabluary.getWord('Project Contributors'),
-                        style: Theme.of(context).textTheme.caption,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Divider(
-                height: 0,
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 5),
-                child: Column(
-                  children: <Widget>[
-                    InfoLabel("Вклад в разработку",
-                        description: "↑154 ↓50 строк кода.",
-                        icon: MdiIcons.codeTags),
-                    InfoLabel("Денежный вклад",
-                        description: "\$50.4 в сумме.", icon: MdiIcons.coin),
-                  ],
-                ),
-              ),
-              Divider(
-                height: 0,
-              ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(25, 2, 25, 0),
-                child: TabBar(
-                  labelColor: Theme.of(context).primaryColor,
-                  unselectedLabelColor: Theme.of(context).primaryColorDark,
-                  indicator: MD2Indicator(
-                    indicatorHeight: 3,
-                    indicatorColor: Theme.of(context).primaryColor,
-                    indicatorSize: MD2IndicatorSize.normal,
                   ),
-                  tabs: [
-                    Tab(
-                      child: Text("Разработчики"),
+                  Divider(
+                    height: 0,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 5),
+                    child: Column(
+                      children: <Widget>[
+                        InfoLabel("Вклад в разработку",
+                            description: "↑154 ↓50 строк кода в сумме.",
+                            icon: MdiIcons.codeTags),
+                        InfoLabel("Денежный вклад",
+                            description: "\$50.4 в сумме.",
+                            icon: MdiIcons.coin),
+                      ],
                     ),
-                    Tab(
-                      child: Text("Пожертвование"),
+                  ),
+                  Divider(
+                    height: 0,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(25, 2, 25, 0),
+                    child: TabBar(
+                      labelColor: Theme.of(context).primaryColor,
+                      unselectedLabelColor: Theme.of(context).primaryColorDark,
+                      indicator: MD2Indicator(
+                        indicatorHeight: 3,
+                        indicatorColor: Theme.of(context).primaryColor,
+                        indicatorSize: MD2IndicatorSize.normal,
+                      ),
+                      tabs: [
+                        Tab(
+                          child: Text("Разработчики"),
+                        ),
+                        Tab(
+                          child: Text("Пожертвование"),
+                        ),
+                      ],
+                      controller: controllerTab,
                     ),
-                  ],
-                  controller: controllerTab,
-                ),
+                  ),
+                ],
               ),
+              Container(
+                color: Theme.of(context).scaffoldBackgroundColor,
+                child: Center(
+                  child: Column(
+                    children: [Text("аааааааа")],
+                  ),
+                ),
+              )
             ],
           ),
         ),
@@ -113,7 +128,44 @@ class _ContributorsPageState extends State<ContributorsPage>
         children: [
           appBar(context),
           TabBody(
-            Text("А в чом смысол"),
+            custom.Card(
+              Column(
+                children: <Widget>[
+                  ContributorLabel(
+                    "Semyon Butenko",
+                    nickname: "Laim0n",
+                    avatarUrl:
+                        'https://sun1-84.userapi.com/c856136/v856136189/7345e/Ht4mdQUJuu0.jpg',
+                    additions: 100,
+                    deletions: 22,
+                  ),
+                  ContributorLabel(
+                    "Semyon Butenko",
+                    nickname: "Laim0n",
+                    avatarUrl:
+                        'https://sun1-84.userapi.com/c856136/v856136189/7345e/Ht4mdQUJuu0.jpg',
+                    additions: 100,
+                    deletions: 22,
+                  ),
+                  ContributorLabel(
+                    "Semyon Butenko",
+                    nickname: "Laim0n",
+                    avatarUrl:
+                        'https://sun1-84.userapi.com/c856136/v856136189/7345e/Ht4mdQUJuu0.jpg',
+                    additions: 100,
+                    deletions: 22,
+                  ),
+                  ContributorLabel(
+                    "Semyon Butenko",
+                    nickname: "Laim0n",
+                    avatarUrl:
+                        'https://sun1-84.userapi.com/c856136/v856136189/7345e/Ht4mdQUJuu0.jpg',
+                    additions: 100,
+                    deletions: 22,
+                  ),
+                ],
+              ),
+            ),
           ),
         ],
       ),
