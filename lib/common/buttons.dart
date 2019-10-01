d6cf7b3b5e566c6c69abb924050f08e5869fec81
Eugene Malinowsky <mandelstamx@icloud.com>
2019-10-01 16:33:32 +0300

    UI fixes

--- lib/common/buttons.dart
+++ lib/common/buttons.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+
 class IconButton extends StatelessWidget {
   final double width;
   final double height;
