aea26a07dc7af4770e745968211ab5f0379b17bf
putin <help.injective@gmail.com>
2019-10-06 20:42:54 +0300

    Fix

--- lib/main.dart
+++ lib/main.dart
@@ -52,8 +52,6 @@ Future setTheme(BuildContext c) async
 
   if (settings.isVirtual) {
     changeTheme(false, c);
-
-    settings.setItem("is_dark", false);
     await settings.saveContainer();
   }
   else {
